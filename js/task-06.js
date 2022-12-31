6f1c0fe7d974f568f12b828afd4c0ccddfd530e4
AkiraReborn <98488464+AkiraReborn@users.noreply.github.com>
2022-12-31 19:31:07 +0200

    update-06task

--- js/task-06.js
+++ js/task-06.js
@@ -2,10 +2,22 @@ const textInput = document.querySelector("#validation-input");
 
 const succesInput = document.querySelector("#validation-input.valid");
 
-textInput.addEventListener("blur", () => {
-  if (textInput.value.length > 6) {
-    return textInput.classList.add("invalid");
-  } else {
-    return textInput.classList.replace("invalid", "valid");
+// textInput.addEventListener("blur", () => {
+//   if (textInput.value.length === 6) {
+//   textInput.classList.add("valid");
+//   } else {
+//   textInput.classList.add("invalid");
+//   }
+// });
+
+
+textInput.addEventListener('blur', () => {
+  if (textInput.value.length === 6) {
+    textInput.classList.remove('invalid');
+    textInput.classList.add('valid');
   }
+  else {
+    textInput.classList.remove('valid');
+    textInput.classList.add('invalid');
+  };
 });
